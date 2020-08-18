7ec85ee822729f0a8eed572a77839f12115528aa
carolynembulwa <carolynembulwa@gmail.com>
2020-08-18 06:47:25 +0300

    binary question

---
+++ binary.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+
+ Question
+
+ Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+
+Return the sum of these numbers.
+
+ 
+
+Example 1:
+
+
+
+Input: [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+ 
+
+Note:
+
+The number of nodes in the tree is between 1 and 1000.
+node.val is 0 or 1.
+The answer will not exceed 2^31 - 1.
+
+ */
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+         if(root == null) 
+        return 0;
+ 
+    return dfs(root, 0, 0);
+      
+    }
+
+
+public int dfs(TreeNode node, int num, int sum){
+    if(node == null) return sum;
+ 
+    num = num*2 + node.val;
+ 
+    // leaf
+    if(node.left == null && node.right == null) {
+        sum += num;
+        return sum;
+    }
+ 
+    // left subtree + right subtree
+    sum = dfs(node.left, num, sum) + dfs(node.right, num, sum);
+    return sum;
+}
+}
